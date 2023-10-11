0d54356ff2a76af262fdf4107e040108901294e6
Sjors Lockhorst <lockhorstsjors@gmail.com>
2023-10-11 19:07:26 +0200

    Looked around and tried some stuff

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Neural Network from scratch in Rust
 
-This repo contains a completely vanilla implementation of a fully connected neural network.
+This repo contains a 'from scratch' implementation of a fully connected neural network.
+It's only dependency is the [ndarray](https://docs.rs/ndarray/latest/ndarray/) package of Rust.
 It's a personal project to practice with Rust, and to build a NN from the ground up from first principles.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use ndarray::{Array, Ix1};
+
+extern crate ndarray;
+
+trait Layer<D> {
+    fn forward(&self, input: Array<f64, D>) -> Array<f64, D>;
+}
+pub struct InputLayer {
+    input_size: usize
+}
+
+impl Layer<Ix1> for InputLayer {
+    fn forward(&self, input: Array<f64, Ix1>) -> Array<f64, Ix1> {
+        // Placeholder implementation; you'd have the real logic here.
+        input
+    }
+}
+
+
+
+pub struct DenseLayer {
+    input_size: usize,
+    output_size: usize,
+    weights: Vec<f32>,
+    biases: Vec<f32>,
+}
+
+pub struct SoftmaxLayer {
+    size: usize
+}
+
+
+
+
 fn main() {
     print!("Hello world!\n")
 }
