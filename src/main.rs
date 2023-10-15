f496ec1eae1058a26b31e690f930227cadf5d719
Sjors Lockhorst <lockhorstsjors@gmail.com>
2023-10-15 14:44:42 +0200

    Added activation layers to DenseLayers and created first version of a model

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ use rand::distributions::{Distribution, Uniform};
 use ndarray::{Array, Array1, Array2};
 use std::f64::consts::E;
 
+// Rand util
+fn rand_array1(len: usize, min: f64, max: f64) -> Array1<f64> {
+        let dist = Uniform::from(min..max);
+        let mut rng = rand::thread_rng();
+        Array::from_shape_fn(len, |_| dist.sample(&mut rng))
+}
 
 fn rand_array2(shape: (usize, usize), min: f64, max: f64) -> Array2<f64> {
         let dist = Uniform::from(min..max);
@@ -12,43 +18,55 @@ fn rand_array2(shape: (usize, usize), min: f64, max: f64) -> Array2<f64> {
         Array::from_shape_fn(shape, |_| dist.sample(&mut rng))
 }
 
-trait Layer<A> {
+// Traits
+pub trait Layer<A> {
     fn forward(&self, input: Array1<A>) -> Array1<A>;
 }
 
-
-pub struct DenseLayer {
-    weights: Array2<f64>,
-    biases: Array1<f64>,
+pub trait Activation<A> {
+    fn apply(&self, input: Array1<A>) -> Array1<A>;
 }
 
-impl DenseLayer {
-    pub fn new(input_size: usize, output_size: usize) -> Self {
-        let weights = rand_array2((input_size, output_size), 0.0, 1.0);
-        let biases: Array1<f64> = Array1::<f64>::ones(output_size);
-
-        DenseLayer {
-            weights,
-            biases
-        }
-    }
-}
-
-impl Layer<f64> for DenseLayer {
-    fn forward(&self, input: Array1<f64>) -> Array1<f64> {
-        let result = input.dot(&self.weights);
-        result + &self.biases
-    }
-}
+// Activation functions
+pub struct Sigmoid;
 
 fn sigmoid(z: f64) -> f64 {
     1.0 / (1.0 + (-z).exp())
 }
 
-pub struct SoftmaxLayer;
-pub struct Sigmoid;
+impl Activation<f64> for Sigmoid {
+    fn apply(&self, input: Array1<f64>) -> Array1<f64> {
+        input.map(|&x| sigmoid(x))
+    }
+}
 
-impl Layer<f64> for SoftmaxLayer {
+// Layers
+pub struct DenseLayer<T, A: Activation<T>> {
+    weights: Array2<T>,
+    biases: Array1<T>,
+    activation: A
+}
+impl<A: Activation<f64>> DenseLayer<f64, A> {
+    pub fn new(input_size: usize, output_size: usize, activation: A) -> Self {
+        let weights = rand_array2((input_size, output_size), 0.0, 1.0);
+        let biases: Array1<f64> = Array1::<f64>::ones(output_size);
+
+        DenseLayer {
+            weights,
+            biases,
+            activation
+        }
+    }
+}
+
+impl <A: Activation<f64>> Layer<f64> for DenseLayer<f64, A> {
+    fn forward(&self, input: Array1<f64>) -> Array1<f64> {
+        self.activation.apply(input.dot(&self.weights) + &self.biases)
+    }
+}
+pub struct Softmax;
+
+impl Layer<f64> for Softmax {
     fn forward(&self, input: Array1<f64>) -> Array1<f64> {
         let max_val = input.sum();
         let exps = input.mapv(|x| E.powf(x - max_val));
@@ -57,24 +75,38 @@ impl Layer<f64> for SoftmaxLayer {
     }
 }
 
-impl Layer<f64> for Sigmoid {
-    fn forward(&self, input: Array1<f64>) -> Array1<f64> {
-        input.map(|&x| sigmoid(x))
+// Model
+pub struct Model<T> {
+    layers: Vec<Box<dyn Layer<T>>>
+}
+
+impl<T> Model<T> {
+    pub fn new() -> Self {
+        Model {
+            layers: Vec::new()
+        }
+    }
+
+    pub fn add_layer<L: Layer<T> + 'static>(&mut self, layer: L) {
+        self.layers.push(Box::new(layer));
+    }
+
+    pub fn forward(&self, mut data: Array1<T>) -> Array1<T> {
+        for layer in &self.layers {
+            data = layer.forward(data);
+        }
+        data
     }
 }
 
 fn main() {
-    let input = Array1::<f64>::ones(3);
+    let input = rand_array1(3, -10.0, 10.0);
 
-    let layer1 = DenseLayer::new(3, 10);
-    let layer2 = DenseLayer::new(10, 10);
-    let softmax = SoftmaxLayer;
-    let sigmoid = Sigmoid;
+    let mut model = Model::new();
+    model.add_layer(DenseLayer::new(3, 10, Sigmoid));
+    model.add_layer(DenseLayer::new(10, 10, Sigmoid));
+    model.add_layer(Softmax);
 
-    let act1 = layer1.forward(input);
-    let act1s = sigmoid.forward(act1);
-    let act2 = layer2.forward(act1s);
-    let act2s = sigmoid.forward(act2);
-    let probs = softmax.forward(act2s);
+    let probs = model.forward(input);
     print!("{}\n", probs)
 }
